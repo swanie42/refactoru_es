d8dc384b97d1a16ea55141442220bdca5bfd0103
swanie42 <swanie42@gmail.com>
2016-09-23 20:10:31 +0000

    y progress on loop-practice-1

---
+++ week2/loop-practice-1/index.js
@@ -0,0 +1,32 @@
+// - 0 100 200 300 400 500
+
+
+for(var i = 0; i < 600; i=i+100){
+  console.log(i);
+}
+
+
+
+// - 1 2 4 8 16 32 64
+
+for (var i = 1; i < 65; i=i*2){
+    console.log(i);
+}
+// - 1 1 1 2 2 2 3 3 3
+for (var i = 1; i<4; i++){
+    
+        for (var j = 1; j<4; j++){
+    
+            for (var k = 1; k<4; k++){
+            }
+        }
+        console.log(k);
+}
+
+// - 0 2 4 6 8 10
+for (var i = 0; i < 11; i=i+2){
+    console.log(i);
+}
+// - 3 6 9 12 15
+// - 9 8 7 6 5 4 3 2 1 0
+// - 0 1 2 3 0 1 2 3 0 1 2 3
