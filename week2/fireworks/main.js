b68098c68110ab807cc00e1b6bec980635b4ff02
swanie42 <swanie42@gmail.com>
2016-09-23 15:01:01 +0000

    Message

--- week2/fireworks/main.js
+++ week2/fireworks/main.js
@@ -1,3 +1,14 @@
 var rocket = document.createElement('div')
 rocket.style.left   = 50vw;
 rocket.style.bottom = 0vh;
+
+var now = performance.now(){
+    
+}
+
+
+var then = performance.now(){
+    
+}
+
+var timeElapsed = then - now
---
+++ week2/function-practice-1/index.js
@@ -0,0 +1,21 @@
+ //Write a function called 'tripleFive' which loops three times using a for loop. Each iteration of the loop, output 'Five!' using console.log.
+    // tripleFive() 
+    //Five! Five! Five!
+function tripleFive () {
+    for (var i = 0; i < 3; i++) {
+        console.log("Five!")
+    }
+
+}
+
+tripleFive(); 
+    
+/*
+- Write a function called 'lastLetter' which takes a single string argument and returns the last character in the string.
+    - lastLetter('hello') should return 'o'
+    - lastLetter('island') should return 'd'
+*/
+function lastLetter(name){
+    return lastLetter.charAt(-1)
+}
+lastLetter();
