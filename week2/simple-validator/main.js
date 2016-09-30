447a67305f63ae38810c0688992f4796cc57bdde
swanie42 <swanie42@gmail.com>
2016-09-30 15:51:29 +0000

    New commit on simple-validator code not quite working

--- week2/simple-validator/main.js
+++ week2/simple-validator/main.js
@@ -26,19 +26,39 @@
 //     - postal code
 //     - must follow the format xxxxx OR xxxxx-xxxx
 
-var postalCode = prompt("Enter your postal code with only 5 or 10 characters");
-
-    if(postalCode.toString().length !== 5 || 
-        postalCode.toString().length !== 10){
-        alert("Try again! Only 5 characters please!");
-    }
-    else{
-        alert("Success!!!!");
-    }
+// var postalCode = prompt("Enter your postal code with only 5 or 10 characters");
+// var newPostalCode = postalCode.toString();
+//     if(newPostalCode.length !== 5 || 
+//         newPostalCode.length !== 10){
+//         alert("Try again! Only 5 characters please!");
+//     }
+//     else{
+//         alert("Success!!!!");
+//     }
     
 //     - state abbreviation
 //     - must be two characters
 //     - must be all caps
+
+// var stateAbv = prompt("Enter your state with as two characters");
+//     var newStateAbv = stateAbv.toString();
+//     if(newStateAbv.length > 2 && newStateAbv.isUpperCase()){
+//         alert("Try again! Only 2 characters and all uppercase please!");
+//     }
+//     else{
+//         alert("Success!!!!");
+//     }
+
+
 //     - married
 //     - must be yes or no
 //     - may be any capitalization: YES, Yes, yes
+
+var married = prompt("Are you married?");
+    var newMarried = married.toString();
+    if(newMarried == "yes" || "no"){
+        alert("Try again! Must be yes or no!");
+    }
+    else{
+        alert("Success!!!!");
+    }
