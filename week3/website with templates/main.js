91564f5adecc432124f2cd3420483c9f5562b60d
Jeffrey Swanson <jeffreyswanson@Jeffreys-MacBook-Pro-3.local>
2016-10-07 13:38:34 -0600

    website-with-template update

--- week3/website with templates/main.js
+++ week3/website with templates/main.js
@@ -1,103 +1,54 @@
-angular.module("Watapp", [])
-    .controller("parkingCtrl", parkingController)
-    .controller("restaurantCtrl", restaurantController)
-    .factory("locationFact", locationFactory);
+angular.module('CremeFraiche', ['ngRoute'])
+    .controller("SoFreshAndSoCremeCreme", cremeController)
+    .controller("CoffeeOrigins", originsController);
 
-parkingController.$inject = ["locationFact"];
-restaurantController.$inject = ["locationFact"];
+angular.module('CremeFraiche')
+    .config(myRouter);
 
-function parkingController(locationFact) {
-    var park = this;
-    park.greeting = "HELLO";
+myRouter.$inject = ['$routeProvider'];
 
-    park.lots = locationFact.lots;
-    park.restaurants = locationFact.restaurants;
+function myRouter($routeProvider) {
 
-    park.filterLots = function(element, index) {
-        // if(element.capacity > 30){
-        //     return true;
-        // }
-        // else {
-        //     return false;
-        // }
-
-        return element.capacity > 30;
-    }
-}    
-
-function restaurantController(locationFact) {
-    var rest = this;
-    rest.greeting = "HELLO2";
-
-    rest.restaurants = locationFact.restaurants;
-    rest.lots = locationFact.lots;
-
-    rest.restFilter = {};
-
-    rest.getNearestLot = function(index) {
-        return rest.lots[index].location;
-    }
-}
-
-function locationFactory() {
-    var lotsData = [
-        {
-            capacity: 20, 
-            cost: 10.00,
-            location: "Larimer Square"
-        },
-        {
-            capacity: 100, 
-            cost: 3.50,
-            location: "Loch Ness"
-        },
-        {
-            capacity: 50, 
-            cost: 5.00,
-            location: "Downtown"
-        },
-    ];
-
-    var restaurantsData = [
-        {
-            name: "Pioneer Pete's",
-            menu: ["Pioneer Pizza", "Heart Attack Burger"],
-            style: "American"
-        },
-        {
-            name: "Hooter's",
-            menu: ["Wings", "Beer", "Nachos"],
-            style: "Blue Collar"
-        },
-        {
-            name: "Linger",
-            menu: ["Sliders", "Goat Cheese Wrapped in Bacon on a Stick"],
-            style: "Really nasty (But actually really good)"
-        },
-    ];
-
-    // The only thing you absolutely have to have in a factory is a RETURN statement
-    return {
-        lots: lotsData,
-        restaurants: restaurantsData,
-    }; // it is very common to return an object here
+    $routeProvider
+        .when('/', {
+            templateUrl: '/templates/home.html'
+        })
+        .when('/origins', {
+            templateUrl: '/templates/origins.html',
+            controller: "CoffeeOrigins as oCtrl"
+        })
+        .when('/menu', {
+            templateUrl: '/templates/menu.html'
+        })
+        .otherwise({
+            redirectTo: '/index.html'
+        })
 }
 
 
-// Filters
-// Filters are used to FORMAT values
-    // they take an input value and provide a transformed output
+function cremeController() {
+    var cCtrl = this;
 
-// Built in filters:
-// currency
-// number
-// date
-// uppercase
-// lowercase
+    cCtrl.greeting = "Welcome to Bozo Coffee";
 
-// Special built in filters:
-// orderBy
-// filter
+    cCtrl.menu = [
+        {
+            name: "White chocolate mocha",
+            price: 7.50
+        },
+        {
+            name: "Black death",
+            price: 6.66
+        },
+        {
+            name: "Non-fat soy vanilla tofu latte with gluten free coffee and free trade, imitation, dolphin-safe whipped-style cream product. Just a sprinkling of GMOs and served in a all-natural 125% post-consumer recycled material hemp cup. Also comes with farm to table wifi",
+            price: 20
+        }
+    ]
+}
 
-// {{ expression | filter:args }}
-// args are generally optional
+function originsController() {
+    var oCtrl = this;
+
+    oCtrl.greeting = "Origins";
+}
