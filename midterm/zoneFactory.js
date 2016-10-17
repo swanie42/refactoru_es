2ac8be2398f459efaa2ae65d1406c97b5b179bfe
Jeffrey Swanson <jeffreyswanson@Jeffreys-MacBook-Pro-3.local>
2016-10-17 09:41:19 -0600

    some midterm stuff and some practices

--- midterm/zoneFactory.js
+++ midterm/zoneFactory.js
@@ -8,7 +8,7 @@ function zoneFactory() {
     zones: [
         {
           img:"http://animalnewyork.com/wp-content/uploads/domino-sugar-factory-continued.jpg"
-          ,name: "Domino Sugar"
+          ,name: "Zone 1"
           ,estSquareFeet: 300000
           ,state: "WI"
           ,margin: 35
@@ -16,7 +16,7 @@ function zoneFactory() {
         },
         {
           img:"https://upload.wikimedia.org/wikipedia/commons/2/2a/Pepsi_Center,_Denver.jpg"
-          ,name: "Pepsi Center"
+          ,name: "Zone 2"
           ,estSquareFeet: 300000
           ,state: "CO"
           ,margin: 22
@@ -24,25 +24,9 @@ function zoneFactory() {
         },
         {
           img:"https://3dprint.com/wp-content/uploads/2016/01/strykerhq.png"
-          ,name: "Printer Facility"
+          ,name: "Zone 3"
           ,estSquareFeet: 300000
           ,state: "AZ"
-          ,margin: 22
-          ,complete: 100
-        },
-        {
-          img:"http://forteandtablada.com/site/wp-content/uploads/2016/04/STAFA-900x600.jpg"
-          ,name: "St. Amant HS"
-          ,estSquareFeet: 300000
-          ,state: "LA"
-          ,margin: 22
-          ,complete: 100
-        },
-        {
-          img:"http://www.neumannsmith.com/wp-content/uploads/2015/06/WSU-STUDENT-CENTER-JD-9.jpg"
-          ,name: "Student Center"
-          ,estSquareFeet: 300000
-          ,state: "MI"
           ,margin: 22
           ,complete: 100
         },
